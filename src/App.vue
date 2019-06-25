9225990228d2ddf3f0b9b6529a46b3e997805530
magerchuang <yb-m11085@163.com>
2019-06-25 19:32:33 +0800

    add createComponents

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<!--keep-live缓存-->
+  <keep-alive>
     <router-view/>
-  </div>
+  </keep-alive>
 </template>
 
 <style lang="scss">
@@ -15,15 +12,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
