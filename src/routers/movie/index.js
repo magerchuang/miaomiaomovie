8af188b66e5078f92a74ec8cfdce82a5adaa1096
magerchuang <yb-m11085@163.com>
2019-07-02 18:25:09 +0800

    addDetail

--- src/routers/movie/index.js
+++ src/routers/movie/index.js
@@ -19,9 +19,25 @@ export default {
       component: () => import('@/components/Search')
     },
     {
-      path: 'detail',
+      path: 'detail/1/:movieId',
       components: {
+        default: () => import('@/components/NowPlaying'),
         detail: () => import('@/views/Movie/detail')
+      },
+      props: {
+        //若是component:则prop:true直接为true即可，而此处是components 有多个router-view 所以对单个使用true 然后解耦
+        detail: true
+      }
+    },
+    {
+      path: 'detail/2/:movieId',
+      components: {
+        default: () => import('@/components/ComingSoon'),
+        detail: () => import('@/views/Movie/detail')
+      },
+      props: {
+        //若是component:则prop:true直接为true即可，而此处是components 有多个router-view 所以对单个使用true 然后解耦
+        detail: true
       }
     },
     {
